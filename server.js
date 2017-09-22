388eb4427dfd5f2e672ae8b8ab8cfd4b127e29f7
tien <tien2609@gmail.com>
2017-09-22 23:32:19 +0700

    fix format

--- server.js
+++ server.js
@@ -40,17 +40,67 @@ io.sockets.on('connection', function (socket) {
       io.emit('updateUserOnline',clients);   
     }else{    
       socket.emit('nameError');  
-    }  })
-    socket.on('connectTo', function(data){ 
-      if(data.to.available)     
-      socket.broadcast.to(data.to.id).emit('isAcceptUser', data.from);   
-      else {    
-        socket.emit('userInGame');
-      }  })
-      io.emit('numConnection',client_count);
-      socket.on('msg', function(data){  
-        io.to(data.room).emit('newmsg', data.info); 
-      })
-      socket.on('I-am-ready', function(){     var thisid = socket.id;     clients.push(thisid);     clients[thisid] = '';     for(var val of clients){       if(val!= thisid && clients[val]==''){         console.log("finded!");         socket.join(val);         clients[val] = val;         clients[thisid] = val;         io.to(val).emit('setRoom', val);         break;       }     }  })  socket.on('restartG', function(data) {    socket.broadcast.to(data).emit('restartgame');  })
-      socket.on('winner', function(data){    io.to(data.room).emit('setwin',data.turn);  })  socket.on('check',function(data){    socket.broadcast.to(data.room).emit('mark',data.pos);  })  socket.on('findnew', function(data){    var thisid = socket.id;    clients[thisid] = '';    socket.broadcast.to(clients[thisid]).emit('LeaveRoom');    for(var val of clients){      if(clients[val]==data){        clients[val]='';      }    }    socket.leave(data);  });   socket.on('disconnect', function(){     console.log("1 user has disconnect");     var thisid = socket.id,i;    /*  socket.broadcast.to(clients[thisid]).emit('LeaveRoom');*/     for(i=0; i<client_count ; i++){       if(clients[i] && clients[i].id==thisid){         break;       }     }     clients.splice(i,1);     userName.splice(i,1);     client_count--;     io.emit('updateUserOnline',clients);   });});
-      server.listen(process.env.PORT || 3000);
+    } 
+  })
+  socket.on('connectTo', function(data){ 
+    if(data.to.available)     
+    socket.broadcast.to(data.to.id).emit('isAcceptUser', data.from);   
+    else {    
+      socket.emit('userInGame');
+    } 
+  })
+  io.emit('numConnection',client_count);
+  socket.on('msg', function(data){  
+    io.to(data.room).emit('newmsg', data.info); 
+  })
+  socket.on('I-am-ready', function(){ 
+    var thisid = socket.id;   
+    clients.push(thisid);   
+    clients[thisid] = '';    
+    for(var val of clients){  
+      if(val!= thisid && clients[val]==''){
+        console.log("finded!");      
+        socket.join(val);      
+        clients[val] = val;    
+        clients[thisid] = val;
+        io.to(val).emit('setRoom', val); 
+        break; 
+      }   
+    } 
+  }) 
+  socket.on('restartG', function(data) { 
+    socket.broadcast.to(data).emit('restartgame'); 
+  })
+  socket.on('winner', function(data){ 
+    io.to(data.room).emit('setwin',data.turn); 
+  }) 
+  socket.on('check',function(data){  
+    socket.broadcast.to(data.room).emit('mark',data.pos);
+  }) 
+  socket.on('findnew', function(data){  
+    var thisid = socket.id;   
+    clients[thisid] = '';   
+    socket.broadcast.to(clients[thisid]).emit('LeaveRoom'); 
+    for(var val of clients){    
+      if(clients[val]==data){  
+        clients[val]='';   
+      }   
+    }   
+    socket.leave(data); 
+  });  
+  socket.on('disconnect', function(){ 
+    console.log("1 user has disconnect");  
+    var thisid = socket.id,i;   
+    /*  socket.broadcast.to(clients[thisid]).emit('LeaveRoom');*/ 
+    for(i=0; i<client_count ; i++){     
+      if(clients[i] && clients[i].id==thisid){  
+        break;      
+      }    
+    }   
+    clients.splice(i,1);   
+    userName.splice(i,1); 
+    client_count--;   
+    io.emit('updateUserOnline',clients);
+  });
+});
+server.listen(process.env.PORT || 3000);
